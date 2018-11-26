ac2078aa72bebf846f76abdeeff38f50bc65a435
Anthony Lamot <anthony.lamot@gmail.com>
2018-11-26 10:56:06 +0100

    adding chai and mocha for testing

--- tests/oauth.js
+++ tests/oauth.js
@@ -1,3 +1,17 @@
-const config = require('../config/index.js');
+const expect = require('chai').expect;
+const config = require('../config');
+const sfmc = require('../sfmc');
 
-console.log(config);
+console.log({ config });
+
+sfmc.soap.oauth(config, (err, result, body) => {
+  console.log({err, result, body});
+});
+
+// ZcyovBTeVGe07Pd0e3tKrYdc
+
+describe('oauth()', () => {
+  it('should return an accessToken', () => {
+    
+  });
+});
